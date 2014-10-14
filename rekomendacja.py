e65af0609397bd88e6a110cd84d9941fc4d3af39
Paulina B. <paulina.bednar@gmail.com>
2014-10-14 18:32:55 +0200

    podejscie 1
    Zadanie wykonane na pierwszych cwiczeniach

--- rekomendacja.py
+++ rekomendacja.py
@@ -1,6 +1,3 @@
-#  Wzorowane na przykładzie Rona Zacharskiego
-#
-
 from math import sqrt
 
 users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5, "Phoenix": 5.0, "Slightly Stoopid": 1.5, "The Strokes": 2.5, "Vampire Weekend": 2.0},
@@ -13,35 +10,17 @@ users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5
          "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
         }
 
-
-
 def manhattan(rating1, rating2):
-    """Oblicz odległość w metryce taksówkowej między dwoma  zbiorami ocen
-       danymi w postaci: {'The Strokes': 3.0, 'Slightly Stoopid': 2.5}
-       Zwróć -1, gdy zbiory nie mają wspólnych elementów"""
-       
-    # TODO: wpisz kod
-    pass
+    klucze1 = rating1.keys()
+    klucze2= rating2.keys()
+    d = 0
+    
+    for klucz in klucze1:
+        if klucz in rating2.keys():
+            d=d+abs(rating2[klucz]-rating1[klucz])
+        
+    print("Odleglosc miedzy gustami wybranych osob to %f" %d)
+pass
 
 
-def computeNearestNeighbor(username, users):
-    """dla danego użytkownika, zwróć listę innych użytkowników według bliskości preferencji"""
-    distances = []
-    # TODO: wpisz kod
-    return distances
-
-def recommend(username, users):
-    """Zwróć listę rekomendacji dla użytkownika"""
-    # znajdź preferencje najbliższego sąsiada
-    nearest = computeNearestNeighbor(username, users)[0][1]
-
-    recommendations = []
-    # zarekomenduj użytkownikowi wykonawcę, którego jeszcze nie ocenił, a zrobił to jego najbliższy sąsiada
-    # TODO: wpisz kod
-    # using the fn sorted for variety - sort is more efficient
-    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
-
-# przykłady
-
-print( recommend('Hela', users))
-#print( recommend('Celina', users))
+manhattan(users["Fruzia"], users["Gosia"])
