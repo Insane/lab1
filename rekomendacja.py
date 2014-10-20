f9be10ade5582ee641d40c706555cbca5f9b4b19
Paulina B. <paulina.bednar@gmail.com>
2014-10-20 19:27:26 +0200

    lab1 - praca na zajeciach
    20-10-2014r.

--- rekomendacja.py
+++ rekomendacja.py
@@ -7,20 +7,46 @@ users = {"Ania": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones": 4.5
          "Ela": {"Broken Bells": 4.0, "Deadmau5": 1.0, "Norah Jones": 4.0, "The Strokes": 4.0, "Vampire Weekend": 1.0},
          "Fruzia":  {"Broken Bells": 4.5, "Deadmau5": 4.0, "Norah Jones": 5.0, "Phoenix": 5.0, "Slightly Stoopid": 4.5, "The Strokes": 4.0, "Vampire Weekend": 4.0},
          "Gosia": {"Blues Traveler": 5.0, "Broken Bells": 2.0, "Norah Jones": 3.0, "Phoenix": 5.0, "Slightly Stoopid": 4.0, "The Strokes": 5.0},
-         "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0}
+         "Hela": {"Blues Traveler": 3.0, "Norah Jones": 5.0, "Phoenix": 4.0, "Slightly Stoopid": 2.5, "The Strokes": 3.0, "Hunter":1.0,"Metallica":10.0}
         }
-
+ 
 def manhattan(rating1, rating2):
     klucze1 = rating1.keys()
     klucze2= rating2.keys()
     d = 0
-    
+    flaga=False
     for klucz in klucze1:
         if klucz in rating2.keys():
+            flaga=True
             d=d+abs(rating2[klucz]-rating1[klucz])
+    if flaga:
+         return d
+    else:
+         return -1       
+
+def test_manhattan(rating1,rating2,d):
+     if manhattan(rating1,rating2) == d:
+        True
+     else:
+         False
         
-    print("Odleglosc miedzy gustami wybranych osob to %f" %d)
-pass
+def najblizszy_sasiad(username, users):
+    distances = []
+    for uzytkownik in users:
+        if uzytkownik!=username:
+            odleglosc=manhattan(users[username],users[uzytkownik])
+            distances.append((odleglosc,uzytkownik))     
+    return sorted(distances)
 
+def recommend(username,users):
+    nearest=najblizszy_sasiad(username, users)[0][1]
+    recommendations = []
+    rating_of_nearest= users[nearest] 
+    userRating = users[username]
+    for artist in rating_of_nearest:
+        if not artist in userRating:
+            recommendations.append((artist,rating_of_nearest[artist]))       
+    
+    return sorted(recommendations, key=lambda artistTuple: artistTuple[1], reverse = True)
 
-manhattan(users["Fruzia"], users["Gosia"])
+print ("Rekomendacje to: %s" %recommend("Ania",users))
